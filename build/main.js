fb2351b6b1a7e7df99ace3df1543816915ddb55a
OscarRatcliffe <cyberskely@gmail.com>
2023-12-11 22:24:19 +0000

    Working build complete

--- build/main.js
+++ build/main.js
@@ -1,12 +1,30 @@
 "use strict";
 const { app, Tray, Menu } = require('electron');
+const fs = require('fs');
+const { exec } = require('child_process');
 let tray = null;
 app.on('ready', () => {
-    tray = new Tray('../assets/TempIcon.png');
+    tray = new Tray('assets/TempIcon.png');
     function changeIP(IP) {
         console.log(IP);
+        let adapter = "Ethernet 3";
+        let subnet = "255.255.0.0";
+        let gateway = "10.0.0.1";
+        let command = `netsh interface ip set address name='${adapter}' static ${IP} ${subnet} ${gateway}`;
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing command: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`Command error: ${stderr}`);
+                return;
+            }
+            console.log(`Command output: ${stdout}`);
+        });
     }
-    let items = ["10.0.0.12", "192.168.0.2"];
+    const IPlist = JSON.parse(fs.readFileSync('data/IPs.json', 'utf8'));
+    let items = IPlist.main;
     let options = [];
     for (let i = 0; i < items.length; i++) {
         options.push({
@@ -15,8 +33,7 @@ app.on('ready', () => {
             click: () => changeIP(items[i])
         });
     }
-    options.push({ type: 'separator' }, { label: 'Quit', type: 'normal', click: () => app.quit() });
-    // Create a context menu
+    options.push({ label: "DHCP", type: 'radio', click: () => changeIP(null) }, { type: 'separator' }, { label: 'Quit', type: 'normal', click: () => app.quit() });
     const contextMenu = Menu.buildFromTemplate(options);
     tray.setContextMenu(contextMenu);
     tray.setToolTip('Electron Tray App');
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,41 @@
 const { app, Tray, Menu } = require('electron');
+const fs = require('fs');
+const { exec } = require('child_process');
 
 let tray = null;
 
 app.on('ready', () => {
-  tray = new Tray('../assets/TempIcon.png');
 
-  function changeIP(IP:String) {
+  tray = new Tray('assets/TempIcon.png');
+  
+  function changeIP(IP:String | null) {
     console.log(IP)
+
+    let adapter = "Ethernet 3"
+    let subnet = "255.255.0.0"
+    let gateway = "10.0.0.1"
+
+    let command = `netsh interface ip set address name='${adapter}' static ${IP} ${subnet} ${gateway}`
+
+    exec(command, (error:any, stdout:any, stderr:any) => {
+      
+      if (error) {
+        console.error(`Error executing command: ${error.message}`);
+        return;
+      }
+    
+      if (stderr) {
+        console.error(`Command error: ${stderr}`);
+        return;
+      }
+    
+      console.log(`Command output: ${stdout}`);
+    });
   }
 
-  let items: Array<String> = ["10.0.0.12", "192.168.0.2"]
+  const IPlist = JSON.parse(fs.readFileSync('data/IPs.json', 'utf8'));
+
+  let items: Array<String> = IPlist.main 
 
   let options: Array<object> = []
 
@@ -22,11 +48,11 @@ app.on('ready', () => {
   }
 
   options.push(
+    {label: "DHCP", type: 'radio', click: () => changeIP(null) },
     { type: 'separator' },
     { label: 'Quit', type: 'normal', click: () => app.quit() }
   )
 
-  // Create a context menu
   const contextMenu = Menu.buildFromTemplate(options);
 
   (tray as any).setContextMenu(contextMenu);
